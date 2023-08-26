a976694c4bf70b30b8c034af796e398151265fd0
phuongdpham <phamduyphuong10@gmail.com>
2023-08-26 20:47:01 +0700

    init project

---
+++ src/main/java/vn/edu/vtiacademy/vtiassignment5/VtiAssignment5Application.java
@@ -0,0 +1,13 @@
+package vn.edu.vtiacademy.vtiassignment5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtiAssignment5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtiAssignment5Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/vtiacademy/vtiassignment5/VtiAssignment5ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vtiacademy.vtiassignment5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtiAssignment5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
